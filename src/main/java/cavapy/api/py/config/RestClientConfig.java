c605d927158b769b4734688dfd918aac5a314df8
rodrigo7623 <rodrigo7623@gmail.com>
2024-04-25 11:13:21 -0400

    se modifica la manera de leer el archivo con el cert SSL

--- src/main/java/cavapy/api/py/config/RestClientConfig.java
+++ src/main/java/cavapy/api/py/config/RestClientConfig.java
@@ -11,7 +11,8 @@ import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
-import java.io.FileInputStream;
+import java.io.*;
+import java.net.URL;
 import java.security.KeyStore;
 
 @Configuration
@@ -23,10 +24,27 @@ public class RestClientConfig {
     @Value("${server.ssl.key-store-password}")
     String trustedStorePassword;
 
+    ClassLoader classLoader = getClass().getClassLoader();
+
     @Bean
     public RestTemplate restTemplate() throws Exception {
+
+        FileInputStream fileInputStream;
+        File keyFile = null;
+
+        URL resourceUrl = classLoader.getResource("keystore/keystore.p12");
+
+        if (resourceUrl != null) {
+            keyFile = new File(resourceUrl.getFile());
+        }
+
+        if (keyFile != null) {
+            fileInputStream = new FileInputStream(keyFile);
+        } else {
+            fileInputStream = new FileInputStream(trustStore.getFile());
+        }
+
         KeyStore keyStore = KeyStore.getInstance("PKCS12");
-        FileInputStream fileInputStream = new FileInputStream(trustStore.getFile());
         keyStore.load(fileInputStream, "password".toCharArray());
         CloseableHttpClient httpClient = HttpClients.custom().setSSLContext(SSLContextBuilder.create().loadKeyMaterial(
                 keyStore, "password".toCharArray()).build()).build();
